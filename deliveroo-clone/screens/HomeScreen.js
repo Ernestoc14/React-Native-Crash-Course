ec98dbb20e349e40c9c8dbb2d004b1dbb949dc6d
Ernestoc14 <descreed34@hotmail.com>
2023-10-19 23:15:01 -0500

    Feat: Adding the Header Icons and Styling

--- deliveroo-clone/screens/HomeScreen.js
+++ deliveroo-clone/screens/HomeScreen.js
@@ -1,7 +1,13 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,20 +19,37 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-3">
       <Text className="text-yellow-300">
         {/* Header */}
-        <View className="flex-row pb-3 pl-3 items-center mx-4 space-x-2">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image
             source={{
               uri: "https://links.papareact.com/wru",
             }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            className="h-7 w-7 bg-gray-900 rounded-full"
           />
-          <View>
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">Current Location</Text>
+          <View className="flex-1">
+            <Text className="font-bold text-gray-500 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#FF77AA" />
+            </Text>
           </View>
+          <UserIcon size={28} color="#FF77AA" />
+        </View>
+        {/* Search Bar */}
+        <View>
+          <View className="">
+            <SearchIcon />
+            <TextInput
+              placeholder="Restaurants and Cuisines"
+              keyboardType="default"
+            />
+          </View>
+          <AdjustmentsVerticalIcon color="#FF77AA" />
         </View>
       </Text>
     </SafeAreaView>
